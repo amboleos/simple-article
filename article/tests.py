d8db7a3754bb127dd5f95edad14f6e889d385eca
amboleos <efeturhan@gmail.com>
2018-11-19 00:46:49 +0300

    Update tests.py

--- article/tests.py
+++ article/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from .templatetags import article_tags
 from .models import Article
